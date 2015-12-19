a76bef6285dcd6ee8234f2823f2a302e2e59bd56
pykyon <s1550002@jaist.ac.jp>
2015-12-19 17:09:08 +0900

    テストの自動化 - ワーク4まで

--- src/test/java/practicework/PracticeWork1Test.java
+++ src/test/java/practicework/PracticeWork1Test.java
@@ -5,9 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import util.CalendarUtility;
 
 import java.io.File;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.core.Is.is;
@@ -26,42 +27,52 @@ public class PracticeWork1Test extends ChromeDriverTest {
         File html = new File("reserveApp/index.html");
         String url = html.toURI().toString();
         driver.get(url);
-        
-        // TODO 以下は削除してください
-        //Thread.sleep(8000);
-        // TODO ここまで削除してください
-        
-        // 1ページ目入力画面
+
+        // 現在のシステム日付を起点に明日以降の直近の土曜日を取得します
+        Calendar immediateSaturday = CalendarUtility.getImmediateDayOfWeek(Calendar.getInstance(), Calendar.SATURDAY);
+        int reserveYear = immediateSaturday.get(Calendar.YEAR);
+        int reserveMonth = immediateSaturday.get(Calendar.MONTH) + 1;
+        int reserveDay = immediateSaturday.get(Calendar.DAY_OF_MONTH);
+
+        // 上記で取得した直近の土曜日を使用します
         driver.findElement(By.id("reserve_year")).clear();
-        driver.findElement(By.id("reserve_year")).sendKeys("2015"); // TODO 明日以降直近の土曜日に変更してください
+        driver.findElement(By.id("reserve_year")).sendKeys(Integer.toString(reserveYear));
         driver.findElement(By.id("reserve_month")).clear();
-        driver.findElement(By.id("reserve_month")).sendKeys("12"); // TODO 明日以降直近の土曜日に変更してください
+        driver.findElement(By.id("reserve_month")).sendKeys(Integer.toString(reserveMonth));
         driver.findElement(By.id("reserve_day")).clear();
-        driver.findElement(By.id("reserve_day")).sendKeys("26"); // TODO 明日以降直近の土曜日に変更してください
+        driver.findElement(By.id("reserve_day")).sendKeys(Integer.toString(reserveDay));
         driver.findElement(By.id("reserve_term")).clear();
         driver.findElement(By.id("reserve_term")).sendKeys("1");
-
-        // TODO 残りの処理を記述してください
         driver.findElement(By.id("headcount")).clear();
         driver.findElement(By.id("headcount")).sendKeys("9");
         driver.findElement(By.id("breakfast_on")).click();
-        WebElement plan_a = driver.findElement(By.id("plan_a"));
-        if( !plan_a.isSelected() ){
-            plan_a.click();
+        WebElement planA = driver.findElement(By.id("plan_a"));
+        if (!planA.isSelected()) {
+            planA.click();
         }
-        WebElement plan_b = driver.findElement(By.id("plan_b"));
-        if( !plan_b.isSelected() ){
-            plan_b.click();
+        WebElement planB = driver.findElement(By.id("plan_b"));
+        if (!planB.isSelected()) {
+            planB.click();
         }
         driver.findElement(By.id("guestname")).clear();
         driver.findElement(By.id("guestname")).sendKeys("a");
+        driver.findElement(By.id("goto_next")).click();
 
         // 2ページ目入力画面
+        Calendar checkoutSunday = (Calendar) immediateSaturday.clone();
+        checkoutSunday.add(Calendar.DAY_OF_MONTH, 1);
+        int checkoutYear = checkoutSunday.get(Calendar.YEAR);
+        int checkoutMonth = checkoutSunday.get(Calendar.MONTH) + 1;
+        int checkoutDay = checkoutSunday.get(Calendar.DAY_OF_MONTH);
         assertThat(driver.findElement(By.id("price")).getText(), is("105750"));
-        assertThat(driver.findElement(By.id("datefrom")).getText(), is("2015年12月26日")); // TODO 変更してください
-        assertThat(driver.findElement(By.id("dateto")).getText(), is("2015年12月27日")); // TODO 変更してください
+        assertThat(driver.findElement(By.id("datefrom")).getText(), is(reserveYear + "年" + reserveMonth + "月" + reserveDay + "日"));
+        assertThat(driver.findElement(By.id("dateto")).getText(), is(checkoutYear + "年" + checkoutMonth + "月" + checkoutDay + "日"));
         assertThat(driver.findElement(By.id("dayscount")).getText(), is("1"));
-
-        // TODO 残りの処理を記述してください
+        assertThat(driver.findElement(By.id("hc")).getText(), is("9"));
+        assertThat(driver.findElement(By.id("bf_order")).getText(), is("あり"));
+        assertThat(driver.findElement(By.id("plan_a_order")).getText(), is("昼からチェックインプラン"));
+        assertThat(driver.findElement(By.id("plan_b_order")).getText(), is("お得な観光プラン"));
+        assertThat(driver.findElement(By.id("gname")).getText(), is("a"));
+        driver.findElement(By.id("commit")).click();
     }
 }
